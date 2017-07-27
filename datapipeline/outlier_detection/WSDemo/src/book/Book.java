24c8d3fb59a7d24feb14eb6415286d2ebc1a616a
dbasakscc <dbasak@salesforce.com>
2017-07-27 14:19:23 -0400

    restructuring the project src

--- datapipeline/outlier_detection/WSDemo/src/book/Book.java
+++ datapipeline/outlier_detection/WSDemo/src/book/Book.java
@@ -5,10 +5,7 @@ import javax.ws.rs.Path;
 import javax.ws.rs.PathParam;
 import javax.ws.rs.Produces;
 import javax.ws.rs.core.MediaType;
-
-import redis.clients.jedis.Jedis;
-import Server.RedisManager;
-import com.dw.*; //Outlier Detection Service
+import com.dw.*;
 
 @Path("/book")
 public class Book {
@@ -18,10 +15,6 @@ public class Book {
 	
 	@Path("{gname}")
 	public String serve_HTML(@PathParam("gname") String gname) {
-		Jedis jedis = RedisManager.getInstance().getJedis();
-		//String metric = jedis.get("ngroup");
-		RedisManager.getInstance().returnJedis(jedis);
-		
 		App entry = new App();
 		String[] args = null;
 		args = new String[1];
